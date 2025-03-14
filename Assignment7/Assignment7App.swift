952e83bccab3f957aa795ce867ab548edef556c6
Anand Otiv <w0858857@myscc.ca>
2025-03-13 20:34:43 -0400

    Initial Commit

---
+++ Assignment7/Assignment7App.swift
@@ -0,0 +1,17 @@
+//
+//  Assignment7App.swift
+//  Assignment7
+//
+//  Created by Anand Otiv on 2025-03-13.
+//
+
+import SwiftUI
+
+@main
+struct Assignment7App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
